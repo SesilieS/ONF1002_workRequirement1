96a1cac4d5a6f62d93bbc0b5688aa56cbe33d156
Sesilie Solskog <sesilie95@hotmail.com>
2025-03-01 10:58:49 +0100

    refactored file structure

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var saturnImage = document.querySelector("img");
-saturnImage.onmouseover = function () {
-  var modal = document.getElementById("myModal");
-  modal.style.display = "block";
+const saturnImage = document.querySelector("img");
+const modal = document.getElementById("myModal");
+const closeBtn = document.querySelector(".close");
+
+saturnImage.onmouseover = () => {
+  modal.showModal();
 };
 
-var closeBtn = document.getElementsByClassName("close")[0];
-closeBtn.onclick = function () {
-  var modal = document.getElementById("myModal");
-  modal.style.display = "none";
+closeBtn.onclick = () => {
+  modal.close();
 };
 
-window.onclick = function (event) {
-  var modal = document.getElementById("myModal");
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.close();
   }
 };
+
